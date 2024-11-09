506c4a8e1a46184a2e62104068d30a16640f0787
Kz <iamkikevanegas@gmail.com>
2024-11-09 15:03:48 +0100

    feat: ready to an initial ship in

--- controllers/music/playlists.get.js
+++ controllers/music/playlists.get.js
@@ -1,4 +1,7 @@
-const { getAllDataOfCollection } = require("../../db/get.method");
+const {
+  getAllDataOfCollection,
+  getByDocument,
+} = require("../../db/get.method");
 const { AsyncCatcher } = require("../../utils/async-catcher");
 const { AsyncErrorHandler } = require("../../utils/async-error-handler");
 
@@ -26,4 +29,35 @@ const getPlaylists = AsyncCatcher(async (req, res, next) => {
   });
 });
 
-module.exports = { getPlaylists };
+const getSinglePlaylist = AsyncCatcher(async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return next(new AsyncErrorHandler("Missing required parameter", 400));
+  }
+
+  const request = await getByDocument({
+    collection_name: "iamkikevanegas",
+    document_name: req.params.id,
+  });
+
+  if (request.status !== 200) {
+    return next(new AsyncErrorHandler(request.message, request.status));
+  }
+
+  const modelledSongsCollection = request.data.songs.map((item, index) => ({
+    ...item,
+    playlistName: request.data.name,
+    index,
+  }));
+
+  res.status(request.status).json({
+    status: "success",
+    data: {
+      ...request.data,
+      songs: modelledSongsCollection,
+    },
+  });
+});
+
+module.exports = { getPlaylists, getSinglePlaylist };
--- routes/music.route.js
+++ routes/music.route.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const musicRoutes = express.Router();
 
-const { getPlaylists } = require("../controllers/music/playlists.get");
+const {
+  getPlaylists,
+  getSinglePlaylist,
+} = require("../controllers/music/playlists.get");
 const { updatePlaylist } = require("../controllers/music/playlist.update");
 
 musicRoutes.get("/playlists", getPlaylists);
 musicRoutes.put("/playlists/:id", updatePlaylist);
+musicRoutes.get("/playlists/:id", getSinglePlaylist);
 
 module.exports = { musicRoutes };
